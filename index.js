08cc81efd648eb4b13cf71365484f4452c9154d4
allrightsreserved89 <100737435+allrightsreserved89@users.noreply.github.com>
2022-05-18 17:48:34 -0500

    finished prototype work

--- index.js
+++ index.js
@@ -15,8 +15,23 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach= [];
 
+}
+Person.prototype.eat= function(edible){
+  if(this.stomach.length<10){
+    this.stomach.push(edible);
+  }
+}
+
+Person.prototype.poop= function(){
+  this.stomach = []
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
 }
 
 
@@ -36,9 +51,29 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model,mpg) {
+  this.model=model;
+  this.milesPerGallon=mpg;
+  this.tank=0;
+  this.odometer=0;
 }
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
+}
+Car.prototype.drive= function(distance){
+  const drivableDistance = this.tank * this.milesPerGallon;
+  if (dist <= drivableDistance){
+    this.odometer=this.odometer+dist;
+    this.tank = this.tank - (dist/this.milesPerGallon);
+  }else{
+    this.odometer = this.odometer +drivableDistance;
+    this.tank=0;
+    return `I ran out of fuel at ${this.odometer} miles!`
+  }
+}
+
+
+
 
 
 /*
@@ -49,18 +84,24 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name,age,favoriteToy) {
+  Person.call(this,name,age);
+  this.favoriteToy = favoriteToy;
 }
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play= function(){
+  return `Playing with ${this.favoriteToy}`;
+}
+
 
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. in Global scope the value of This refers to the window/console object
+  2. in implicit binding This applies to the Object in reference
+  3. in New Binding This refers to an object created and returned by a Constructor function
+  4. in Explicit binding This is Explicitly defined by using the call or apply method
 */
 
 ///////// END OF CHALLENGE /////////
